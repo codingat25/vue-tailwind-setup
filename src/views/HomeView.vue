7a22a4ce28e8b2183f899a98904529b1d21a19dc
Christian Jay <christianquimporeyes@gmail.com>
2022-06-24 13:51:57 +0800

    setup|

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="justify-center flex bg-yellow-300 items-center h-screen">
+      <div class="text-4xl">
+        Hello 👋🏼
+      </div>
+   </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
